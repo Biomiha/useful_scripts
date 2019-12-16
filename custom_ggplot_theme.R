00e6f2fa364c135099042afbc582b600d23fdcc2
Biomiha <biomiha@gmail.com>
2019-12-16 10:28:26 +0000

    Add files via upload

---
+++ custom_ggplot_theme.R
@@ -0,0 +1,13 @@
+# set ggplot theme
+theme_set(theme_bw() +
+            theme(axis.line = element_blank(),
+                  axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0), size = 14),
+                  axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0), size = 14),
+                  axis.text.x = element_text(size = 12),
+                  axis.text.y = element_text(size = 12),
+                  legend.title = element_text(size = 14), 
+                  legend.text = element_text(size = 12),
+                  strip.text.x = element_text(size = 14),
+                  plot.title = element_text(hjust = 0.5, size = 16),
+                  plot.subtitle = element_text(hjust = 0.5, size = 12),
+                  legend.justification="left"))
