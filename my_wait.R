924df22c554fde062c2805890f69fe30aa2ea2d5
Biomiha <biomiha@gmail.com>
2021-10-04 12:43:48 +0100

    Create my_wait.R

---
+++ my_wait.R
@@ -0,0 +1,10 @@
+library(tcltk)
+
+mywait <- function() {
+  tt <- tktoplevel()
+  tkpack( tkbutton(tt, text='Continue', command=function()tkdestroy(tt)),
+          side='bottom')
+  tkbind(tt,'<Key>', function()tkdestroy(tt) )
+  
+  tkwait.window(tt)
+}
