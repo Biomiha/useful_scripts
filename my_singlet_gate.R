dd6222fbfb0debf133e49e350c17e5e9701ec54e
Biomiha <biomiha@gmail.com>
2023-03-01 08:53:43 +0000

    Update my_singlet_gate.R

--- my_singlet_gate.R
+++ my_singlet_gate.R
@@ -1,6 +1,6 @@
 my_singlet_gate_fun <- function(fr, pp_res = NULL, channels = c("FSC-A", "FSC-H"), ampl_factor = 1.3){
   require(flowCore)
-  exprs_mat <- exprs(ff[, channels])
+  exprs_mat <- exprs(fr[, channels])
   indexes <- exprs_mat[, grep(pattern = "-A", channels)] <= ampl_factor * exprs_mat[, grep(pattern = "-A", channels, invert = TRUE)]
   exprs_mat <- exprs_mat[indexes,]
   hpts <- chull(exprs_mat)
