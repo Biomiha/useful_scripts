ec5c9197e6ad1feed0c1364d0aa57ecf42f3cd1d
Biomiha <biomiha@gmail.com>
2023-03-01 08:51:11 +0000

    Add improved singlet gate

---
+++ my_singlet_gate.R
@@ -0,0 +1,11 @@
+my_singlet_gate_fun <- function(fr, pp_res = NULL, channels = c("FSC-A", "FSC-H"), ampl_factor = 1.3){
+  require(flowCore)
+  exprs_mat <- exprs(ff[, channels])
+  indexes <- exprs_mat[, grep(pattern = "-A", channels)] <= ampl_factor * exprs_mat[, grep(pattern = "-A", channels, invert = TRUE)]
+  exprs_mat <- exprs_mat[indexes,]
+  hpts <- chull(exprs_mat)
+  hpts <- c(hpts, hpts[1])
+  polygonGate(.gate = exprs_mat[hpts,], filterId = "singlets")
+}
+require(openCyto)
+registerPlugins(my_singlet_gate_fun, "my_singlet_gate")
